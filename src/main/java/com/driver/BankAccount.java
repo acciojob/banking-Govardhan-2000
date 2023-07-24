88fc448832928ebbbc0c0ea504c1d62011cd1eb8
Govardhan <kurubagovardhan32@gmail.com>
2023-07-24 10:32:49 +0530

    code added

--- src/main/java/com/driver/BankAccount.java
+++ src/main/java/com/driver/BankAccount.java
@@ -3,7 +3,7 @@ package com.driver;
 public class BankAccount {
 
     private String name;
-    private double balance;
+    public double balance;
     private double minBalance;
 
     public BankAccount(String name, double balance, double minBalance) {
@@ -14,18 +14,41 @@ public class BankAccount {
         //Each digit of an account number can lie between 0 and 9 (both inclusive)
         //Generate account number having given number of 'digits' such that the sum of digits is equal to 'sum'
         //If it is not possible, throw "Account Number can not be generated" exception
+if(sum<0 || sum > 9 * digits ){
+    throw new Exception("Account number can not be generated");
+}
+StringBuilder accountNumber = new StringBuilder();
+        for (int i = 0; i < digits; i++) {
+            if (sum >= 9) {
+                accountNumber.append(9);
+                sum -= 9;
+            } else {
+                accountNumber.append(sum);
+                sum = 0;
+            }
+        }
 
-        return null;
+        if (sum > 0) {
+            throw new Exception("Account Number can not be generated");
+        }
+
+        return accountNumber.toString();
+
     }
 
     public void deposit(double amount) {
         //add amount to balance
+        balance=balance+amount;
 
     }
 
     public void withdraw(double amount) throws Exception {
         // Remember to throw "Insufficient Balance" exception, if the remaining amount would be less than minimum balance
 
+        if (balance - amount < minBalance) {
+            throw new Exception("Insufficient Balance");
+        }
+        balance -= amount;
     }
 
 }
--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -1,19 +1,40 @@
 package com.driver;
 
-public class CurrentAccount extends BankAccount{
-    String tradeLicenseId; //consists of Uppercase English characters only
+public class CurrentAccount extends BankAccount {
+    private String tradeLicenseId; // consists of Uppercase English characters only
 
     public CurrentAccount(String name, double balance, String tradeLicenseId) throws Exception {
-        // minimum balance is 5000 by default. If balance is less than 5000, throw "Insufficient Balance" exception
-
+        // Minimum balance is 5000 by default. If the balance is less than 5000, throw "Insufficient Balance" exception
+        super(name, balance, 5000);
+        this.tradeLicenseId = tradeLicenseId;
     }
 
     public void validateLicenseId() throws Exception {
-        // A trade license Id is said to be valid if no two consecutive characters are same
-        // If the license Id is valid, do nothing
+        // A trade license Id is said to be valid if no two consecutive characters are the same.
+        // If the license Id is valid, do nothing.
         // If the characters of the license Id can be rearranged to create any valid license Id
         // If it is not possible, throw "Valid License can not be generated" Exception
 
-    }
+        char[] chars = tradeLicenseId.toCharArray();
+        for (int i = 1; i < chars.length; i++) {
+            if (chars[i] == chars[i - 1]) {
+                // Rearrange characters to create a valid license ID
+                for (int j = i + 1; j < chars.length; j += 2) {
+                    if (j < chars.length && chars[j] != chars[i]) {
+                        char temp = chars[i];
+                        chars[i] = chars[j];
+                        chars[j] = temp;
+                    }
+                }
+            }
+        }
 
+        tradeLicenseId = new String(chars);
+        // Check if the rearranged license ID is valid
+        for (int i = 1; i < chars.length; i++) {
+            if (chars[i] == chars[i - 1]) {
+                throw new Exception("Valid License can not be generated");
+            }
+        }
+    }
 }
